cb03fcc9a54b05b6288b7e837926a29d28465ca4
Kelly Ortiz Figueiredo <kellyortiz.figueiredo@gmail.com>
2019-05-22 20:25:07 -0300

    Criação da classe de conexão ao banco de dados
    Separação das classes

---
+++ src/projeto_CRUD/ConexaoJavaDb.java
@@ -0,0 +1,41 @@
+import java.sql.*;
+public class ConexaoJavaDb {
+	private String usuario;
+	private String senha;
+	private String host;
+	private int port;
+	private String dbName;
+	private Connection connection;
+	public ConexaoJavaDb(String u, String s, String h, int p, String d) {
+		usuario = u;
+		senha = s;
+		host = h;
+		port = p;
+		dbName = d;
+		connection = null;
+	}
+	public Connection getConnection() throws ConexaoException {
+		if (connection == null) {
+			try {
+				String url;
+				url = "jdbc:derby://" + host + ":" + port + "/" + dbName + ";create=true";
+				Class.forName("org.apache.derby.jdbc.ClientDriver");
+				connection = DriverManager.getConnection(url, usuario, senha);
+			} catch (ClassNotFoundException ex) {
+				throw new ConexaoException("Falha ao carregar driver!");
+			} catch (SQLException ex) {
+				throw new ConexaoException("Falha ao abrir conexão!");
+			}
+ 		}
+		return connection;
+	}
+	public void close() throws ConexaoException {
+		if (connection != null) {
+			try { 
+				connection.close(); 
+			} catch (SQLException ex) { 
+				throw new ConexaoException("Problema ao fechar conexão!");
+			}
+		}
+	}
+}
