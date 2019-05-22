de671ca7e8a3cf185bb3eae2bf1e9af3e95e8d12
Kelly Ortiz Figueiredo <kellyortiz.figueiredo@gmail.com>
2019-05-22 20:21:37 -0300

    Atualização da classe DAO
    Aproveitei e mudei o ano de Date para String

--- src/projeto_CRUD/CarrosDAO.java
+++ src/projeto_CRUD/CarrosDAO.java
@@ -1,96 +1,50 @@
-package projeto_CRUD;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 /**
  *
- * @author Kelly Ortiz e Welker
+ * @author Kelly Ortiz F
  */
-public class CarrosDAO {
-    private PreparedStatement stmC;
-    private PreparedStatement stmR;
-    private PreparedStatement stmU;
-    private PreparedStatement stmD;
+public class Carro {
+private long id;
+    private String modelo;
+    private String marca;
+    private String Ano;
+    private String categoria;
     
-    private Connection conn;
-    private List<Carros> Carros;
+    
+    public Carro(long id, String mod, String ano, String m, String categ) {
+      this.id = id;
+        modelo = mod;
+        marca = m;
+        Ano = ano;
+        categoria = categ;
+    }
+
    
+    public long getId() { return id; }
+    public String getModelo() { return modelo; }
+    public String getMarca() { return marca; }
+    public String getAno(){ return Ano; }
+    public String getCategoria() { return categoria; }
     
-    public CarrosDAO() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            this.conn = DriverManager.getConnection("jdbc:mysql://localhost/Projeto","root","");
-            String url = "jdbc:derby://localhost:1527/Projeto";
-            String usuario ="projeto";
-            String senha = "projeto";
-            this.stmC = this.conn.prepareStatement("INSERT INTO carros(modelo,marca,ano,categoria) VALUES(?,?)",
-                    Statement.RETURN_GENERATED_KEYS);
-            this.stmR = this.conn.prepareStatement("SELECT * FROM carros");
-            this.stmU = this.conn.prepareStatement("UPDATE carros SET modelo=?,marca=?,ano=?,categoria=?WHERE id=?");
-            this.stmD = this.conn.prepareStatement("DELETE FROM carros WHERE id=?");
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
-    }    
-    public void close(){
-        try{
-            this.conn.close();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
     
-     public List<Carros> read() throws SQLException {
-        try {
-            ResultSet rs = this.stmR.executeQuery();
-            
-            List<Carros> carros = new ArrayList<>();
-            
-            while(rs.next()) {
-                Carros c = new Carros();
-                c.setId(rs.getLong("id"));
-                c.setModelo(rs.getString("Modelo"));
-                c.setMarca(rs.getString("Marca"));
-                c.setAno(rs.getDate("Ano"));
-                c.setCategoria(rs.getString("Categoria"));
-                carros.add(c);
-            
-            
-            return Carros;
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+    public void setId(long id) { this.id = id; }
+    public void setModelo(String mod) { modelo = mod; }
+    public void setMarca(String m) { marca = m; }
+    public void setAno(String ano) { Ano = ano; }
+    public void setCategoria(String categ) { categoria = categ; }
+    
+    @Override
+    public String toString() {
+        return "Veiculo: " 
+                + this.modelo+ " - " 
+                + this.marca + " - "
+                + this.Ano + " - "
+                + this.categoria + "(" 
+                + this.id + ")";
     }
-     
- 
-    public Carros create(Carros novoCarro) {
-        try {
-            this.stmC.setString(1, novoCarro.getModelo());
-            this.stmC.setString(2, novoCarro.getMarca());
-            this.stmC.setDate(3, novoCarro.getAno());
-            this.stmC.setString(4, novoCarro.getCategoria());
-            this.stmC.executeUpdate();
-            
-            ResultSet rs = this.stmC.getGeneratedKeys();
-            rs.next();
-            long id = rs.getLong(1);
-            novoCarro.setId(id);
-            
-            return novoCarro;
-        }catch(Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+
 }
